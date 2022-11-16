f98056d87aba5c7d9e4781f592baf391e75b3016
Juxtaposy <thgawronski@gmail.com>
2022-11-16 20:45:53 +0100

    Changed database to be object based

--- Meczystan/gui.py
+++ Meczystan/gui.py
@@ -5,11 +5,42 @@ import PySimpleGUI as sg
 import os
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+#We import pickle to save user objects in a file for database purposes
+import pickle
+
+#Class for users
+class Users:
+    #We keep track of created users in order to avoid name conflicts
+    instances = []
+    #Constructor
+    def __init__(self,name) -> None:
+        self.name = name
+        self.typy_gr = [] #Faza grupowa
+        self.typy_p = []  #Faza pucharowa
+        Users.instances.append(self)
 
 #Some standard layout to start with basic color theme
 sg.change_look_and_feel('LightGrey1')
+
 #Menu definition - template
 menu_def = ['&File', ['&New File', '&Open...','Open &Module','---', '!&Recent Files','C&lose']],['&Save',['&Save File', 'Save &As','Save &Copy'  ]],['&Edit', ['&Cut', '&Copy', '&Paste']]
+
+#Read database file
+def db_read():
+    try:
+        file = open(''.join((__location__, "\\users_data.pkl")),'rb')
+        database = pickle.load(file)
+        file.close()
+        return database
+    except:
+        database = []
+        return database
+#Write database file
+def db_write(database):
+    file = open(''.join((__location__, "\\users_data.pkl")),'wb')
+    pickle.dump(database,file,pickle.HIGHEST_PROTOCOL)
+    file.close()
+
 #Function to open Faza Grupowa window and its layout
 def open_fg():
     layout = [[sg.Text("Faza Grupowa")]]
@@ -19,6 +50,7 @@ def open_fg():
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
     window.close()
+
 #Function to open Drabinka Pucharowa window and its layout
 def open_dp():
     layout = [[sg.Text("Drabinka Pucharowa")]]
@@ -28,6 +60,7 @@ def open_dp():
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
     window.close()
+
 #Function to open Typowanie window and its layout
 def open_t():
     layout = [[sg.Text("Typowanie")]]
@@ -37,35 +70,40 @@ def open_t():
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
     window.close()
-#Function to open Użytkownicy w bazie window and its layout.
-#This function allows for deleting existing users.
-def open_wu():
-    #Dynamic layout building using list comprehension for text and button   
-    try:
-        users = open(''.join((__location__, "\\users_data.txt")),'r')
-        database = [line.rstrip() for line in users]
-    except: 
-        sg.Popup('Baza jest pusta!')
-        return None
-    #TO DO: NEED TO FIGURE OUT DYNAMIC KEY FOR DYNAMIC DELETION OF USERS
 
-    layout = [[[sg.Text(database[i]), sg.Button("Usuń",key="usun")] for i in range(len(database))]]
-    window = sg.Window("Użytkownicy w bazie",layout)
-    while True:
-        event, values = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-        if event == "usun": print('ZAKTUALIZUJ DYNAMICZNE USUWANIE')
-    window.close()
+#Function to open Użytkownicy w bazie window and its layout.
+def open_wu():
+    #Dynamic layout building using list comprehension for text and button
+    database = db_read()   
+    if not database:
+        sg.Popup('Baza jest pusta!')
+    else:
+        layout = [[[sg.Text(i.name), sg.Button("Usuń",key="usun")] for i in database]]
+        window = sg.Window("Użytkownicy w bazie",layout)
+        while True:
+            event, values = window.read()
+            if event == "Exit" or event == sg.WIN_CLOSED:
+                break
+            if event == "usun": print('ZAKTUALIZUJ DYNAMICZNE USUWANIE')
+        window.close()
+
 #Function for adding new users to database file
 def open_du():
     layout = [[sg.Text("Enter username"), sg.InputText()], [sg.Submit(),sg.Cancel()]]
     window = sg.Window("Nowy użytkownik",layout)
     event, values = window.read()
-    users = open(os.path.join(__location__, "users_data.txt"),'a')
-    users.write(values[0]+"\n")
-    users.close()
+    database = db_read()
+    if database:
+        for obj in database:
+            if values[0] == obj.name: 
+                sg.Popup('Nazwa zajęta, wybierz inną!')
+                values[0] = False
+    if values[0]: 
+        obj = Users(values[0])
+        database.append(obj)
+        db_write(database)
     window.close()
+
 #Function for opening user database and loading it into program
 def open_u():
     #Open existing database and load users into program
